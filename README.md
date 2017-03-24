d6c6b300ed4990395dd483ae7f99471b8616470d
huisuan Chung <huisuan Chung>
2017-03-24 14:43:45 +0800

    created dividend payout per year

--- README.md
+++ README.md
@@ -3,5 +3,35 @@
 3. toggle show in years or months only?? (idk)
 
 
+1. change the pv to fv function, such that the time period is always in years, but show if it compounds Monthly (DONE)
+2. make the fv function (done)
+3. make the +-1% function
+4. make dividends
 
-1. make pv to fv function and fv to pv function
+
+NOTE
+cashvalue is there. check if it isNOT  empty before dividends can be calc
+___
+done
+
+1. Function to validate everything before showing green calculate button. (Otherwise put disabled button) (DONE)
+____
+To do in app
+
+Change the time period to only show years. The monthly option is for payment and compounding.
+
+So labels only show year increments. And no need to show month anymore.
+
+Growth rate projection (base growth rate , +-0.5, or +-1 or 2% for optimism).(Like flight plans).
+
+Validation for multiple choice of growth rates. Like you can't choose 0.3+-1.
+
+Dividends. Dividends is based on the basic growth rate.
+N. Means after how many years the dividends can be collected. (Start period). Num period minus offset date. And then percentage of dividends.
+Drop down select of whether dividends are paid out or reinvested. (Still need to press calculate button for dividends) (separate buttons?) if reinvested need different calculation. Maybe stacked graphs(?)
+
+Yes. Separate section for dividends. Dividends is calculated yearly but can add label for the monthly amount (?)
+Reset button for dividends. Clear to zero. Dividends can only be activated if the growth setting us zero to only one value
+
+Ask luke what else he needs.
+Dividends has its own separate y axis. Because it's on a different scale. Bar chart.
--- index.js
+++ index.js
@@ -1,17 +1,34 @@
-{
+
   angular.module("app", ["chart.js"]).controller("AppController", function($scope) {
     $scope.pvToFv = true;
+    $scope.cashValue = [];
     $scope.togglePvToFv = function() {
       $scope.pvToFv = !$scope.pvToFv;
     }
-    $scope.validationTypePeriod = function(string) {
+    $scope.validationCalculation = function (fv, pv, pmt, rate, numPeriod, typePeriod, isPvToFV) {
+      if (isPvToFV==false) {
+        return numberValidation(fv) && numberValidation(pmt) && numberValidation(rate) && numberValidation(numPeriod) && validationTypePeriod(typePeriod);
+
+      } else if (isPvToFV==true) {
+        return numberValidation(pv) && numberValidation(pmt) && numberValidation(rate) && numberValidation(numPeriod) && validationTypePeriod(typePeriod);
+      }
+    };
+    $scope.validationDividend = function (rate, startPeriod, payoutMode) {
+      if ($scope.cashValue.length !=0 ) {
+        return numberValidation(rate) && numberValidation(startPeriod) && validationTypePeriod(payoutMode);
+      } else {
+        return false;
+      }
+    };
+    var validationTypePeriod = function(string) {
       if (string == "" || !string) {
         return false;
       } else {
         return true;
       }
-    }
-    $scope.numberValidation = function(string) {
+    };
+    $scope.validationTypePeriod = validationTypePeriod;
+    var numberValidation = function(string) {
       if (!isNaN(string)) {
         if (string != 0) {
           return true;
@@ -21,7 +38,92 @@
         return false;
       }
 
+    };
+
+    var validationPayoutPeriod = function (startPeriod, numPeriod) {
+      // console.log(startPeriod, numPeriod);
+      if (parseFloat(startPeriod) < parseFloat(numPeriod)) {
+        return true;
+      } else {
+        return false;
+      }
     }
+    $scope.validationPayoutPeriod = validationPayoutPeriod;
+    $scope.numberValidation = numberValidation;
+    $scope.calculateDividends = function(rate, start, payoutMode){
+      var numPeriod = $scope.cashValue.length;
+      if ( numPeriod != 0) {
+
+        var array = [];
+
+        for (var i = 1; i < start; i++) {
+          array.push(0);
+        }
+        console.log(array);
+        for (var i = start-1; i < numPeriod; i++) {
+          array.push(Math.round($scope.cashValue[i]*rate/100));
+        }
+        console.log(array);
+        if (payoutMode=="reinvest") {
+          // create another line chart
+          $scope.data[1] = array;
+        } else if (payoutMode=="payout") {
+          // create a bar chart
+          $scope.data[1] = array;
+        }
+
+
+
+      }
+
+
+
+    };
+    $scope.calculatePVfromFV = function(fv, pmt, rate, numPeriod, typePeriod) {
+      var array = [],
+      labelarray = [],
+      currentAmount = 0;
+
+      console.log(fv, pmt, rate, numPeriod, typePeriod);
+      array.push(fv);
+
+      var currentDate = moment();
+      labelarray.push(currentDate.format("MMM YY"));
+
+      for (var i = 1; i < numPeriod; i++) {
+        // generate data
+        if (currentAmount==0) {
+          currentAmount = parseFloat(fv)+0;
+        }
+
+        // if
+        if (typePeriod == "Annual") {
+          currentAmount = (currentAmount / (1 + rate / 100));
+          currentAmount = currentAmount - parseFloat(pmt);
+          console.log(currentAmount);
+          array.push(Math.round(currentAmount));
+        }
+        else if (typePeriod == "Monthly") {
+          for (var j = 0; j < 12; j++) {
+
+            currentAmount = (currentAmount / (1 + rate / 100));
+            currentAmount = currentAmount - parseFloat(pmt);
+          }
+          console.log(currentAmount);
+          array.push(Math.round(currentAmount));
+
+        }
+        // generate labels
+        currentDate.add(1, 'y');
+        labelarray.push(currentDate.format("MMM YY"));
+
+      }
+      array.reverse();
+      $scope.cashValue = array;
+      $scope.data[0] = array;
+      $scope.labels = labelarray;
+      return array;
+    };
     $scope.calculateFVfromPV = function(pv, pmt, rate, numPeriod, typePeriod) {
       var array = [],
       labelarray = [],
@@ -29,6 +131,7 @@
 
       console.log(pv, pmt, rate, numPeriod, typePeriod);
       array.push(pv);
+
       var currentDate = moment();
       labelarray.push(currentDate.format("MMM YY"));
 
@@ -37,27 +140,34 @@
         if (currentAmount==0) {
           currentAmount = parseFloat(pv)+0;
         }
-        currentAmount = currentAmount + parseFloat(pmt);
-        currentAmount = (currentAmount * (1 + rate / 100));
-        console.log(currentAmount);
-        array.push(currentAmount);
 
-        // generate labels
         // if
         if (typePeriod == "Annual") {
-          currentDate.add(1, 'y');
-          labelarray.push(currentDate.format("MMM YY"));
+          currentAmount = currentAmount + parseFloat(pmt);
+          currentAmount = (currentAmount * (1 + rate / 100));
+          console.log(currentAmount);
+          array.push(Math.round(currentAmount));
         }
         else if (typePeriod == "Monthly") {
-          currentDate.add(1, 'M');
-          labelarray.push(currentDate.format("MMM YY"));
+          for (var j = 0; j < 12; j++) {
+
+            currentAmount = currentAmount + parseFloat(pmt);
+            currentAmount = (currentAmount * (1 + rate / 100));
+          }
+          console.log(currentAmount);
+          array.push(Math.round(currentAmount));
+
         }
+        // generate labels
+        currentDate.add(1, 'y');
+        labelarray.push(currentDate.format("MMM YY"));
 
       }
+      $scope.cashValue = array;
       $scope.data[0] = array;
       $scope.labels = labelarray;
       return array;
-    }
+    };
 
     $scope.labels = ["Current Time"];
     $scope.series = ["Series A"];
@@ -73,6 +183,22 @@
     $scope.onClick = function(points, evt) {
       console.log(points, evt);
     };
+    $scope.datasetOverride = [
+      {
+        label: "Total Cash Value",
+        borderWidth: 3,
+        hoverBackgroundColor: "rgba(255,99,132,0.4)",
+        hoverBorderColor: "rgba(255,99,132,1)",
+        type: 'line'
+      },
+  {
+    label: "Dividend Payout Per Year",
+    borderWidth: 1,
+    type: 'bar'
+  }
+
+];
+$scope.colors = ['#45b7cd', '#ff6384', '#ff8e72'];
     // $scope.datasetOverride = [{ yAxisID: 'y-axis-1' }, { yAxisID: 'y-axis-2' }];
     // $scope.options = {
     //   scales: {
@@ -93,5 +219,3 @@
     //   }
     // };
   });
-
-}
